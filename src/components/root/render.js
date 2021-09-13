81f9c5fd3f8bc83e2bc4f8cda4199e79203588e7
Fernando Camargo <camargodelbuono@gmail.com>
2021-09-13 05:19:41 +0200

    ✨

--- src/components/root/render.js
+++ src/components/root/render.js
@@ -1,12 +1,28 @@
-import React from 'heatug/src/macros/core/root/macro';
+import React, {
+  useCallback,
+  useState,
+} from 'heatug/src/macros/core/root/macro';
 import { ThemeProvider as Theming } from '@mui/material/styles';
 
 import theme from 'themes/default';
 import { App, Style } from 'components';
 
-export default () => (
-  <Theming theme={theme}>
-    <Style />
-    <App />
-  </Theming>
-);
+export const increase = (value) => value + 1;
+
+export default () => {
+  const [counter, setCounter] = useState(1);
+  const onClick = useCallback(() => setCounter(increase), []);
+
+  return (
+    <>
+      <Theming theme={theme}>
+        <Style />
+        <App />
+      </Theming>
+      <div>
+        <p>Counter: {counter}</p>
+        <button onClick={onClick}>Increase</button>
+      </div>
+    </>
+  );
+};
