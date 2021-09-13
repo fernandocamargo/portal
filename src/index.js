8d4f1a8280ad7f0ba8acff8f1c6d35e17faeccc1
Fernando Camargo <camargodelbuono@gmail.com>
2021-09-13 06:03:23 +0200

    ✨

--- src/index.js
+++ src/index.js
@@ -4,12 +4,10 @@ import { render } from 'react-dom';
 import { Root } from 'components';
 import { measure } from 'tools';
 
-import pckg from '../package.json';
-
 export const initialize = () =>
   render(
     <StrictMode>
-      <Root pckg={pckg} />
+      <Root />
     </StrictMode>,
     document.getElementById('root'),
     measure.bind(this, process.env.DEBUGGING && console.log)
